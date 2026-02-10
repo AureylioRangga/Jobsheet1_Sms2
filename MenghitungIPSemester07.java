849d5e579ef751c7f65637117218a677842734af
Aureylio <aureyliorangga2@gmail.com>
2026-02-10 11:23:33 +0700

    Pertanyaan1 Pemilihan

--- MenghitungIPSemester07.java
+++ MenghitungIPSemester07.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class MenghitungIPSemester {
+public class MenghitungIPSemester07 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
